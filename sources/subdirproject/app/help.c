f181cbb809749f6fe211858c080c87295ad3f483
user <user@debian>
2015-11-10 22:20:07 +0300

    немного добавил фишек с arg c/v

---
+++ sources/subdirproject/app/help.c
@@ -0,0 +1,7 @@
+#include "help.h"
+
+help::help()
+{
+
+}
+
---
+++ sources/subdirproject/app/help.h
@@ -0,0 +1,15 @@
+#ifndef HELP_H
+#define HELP_H
+
+
+class help
+{
+public:
+    help();
+
+signals:
+
+public slots:
+};
+
+#endif // HELP_H
