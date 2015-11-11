1fcc82df1a7bd6e8523dcf09863291b98e2dd0eb
Anton <antonlamtev@gmail.com>
2015-11-11 22:14:30 +0300

    борюсь с конфликтами 2

--- sources/subdirproject/app/help.c
+++ sources/subdirproject/app/help.c
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 void help(void)
 {
     puts("Помощь!!!");
-=======
-#include "help.h"
 
-help::help()
-{
-
->>>>>>> f181cbb809749f6fe211858c080c87295ad3f483
 }
-
