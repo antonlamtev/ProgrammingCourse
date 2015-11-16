e95c02581d38456579acb3a8dc9a8a1564405209
Anton <antonlamtev@gmail.com>
2015-11-16 21:05:04 +0300

    отладил деление уголком. теперь точно должно работать для всех допустимых входных данных.

--- sources/subdirproject/app/quotient.c
+++ sources/subdirproject/app/quotient.c
@@ -6,5 +6,6 @@ void quotient(void)
     int M, N;
     puts("Введите 2 числа");
     scanf("%i%i", &M, &N);
+    puts("");
     quotient_out(M, N);
 }
--- sources/subdirproject/lib/quotient_process.c
+++ sources/subdirproject/lib/quotient_process.c
@@ -10,14 +10,14 @@ void print_n_symbols(int n, char symbol)
 
 void quotient_out(int M, int N)
 {
-    int dividend, residue, result;
+    int dividend, residue, result, i, product;
 
     result = M / N;
     dividend = M / power(10, numlen(result) - 1);
     residue = M % power(10, numlen(result) - 1);
     int indent = dividend;
 
-    int i;
+    int  crutch = 1; //нужен для правильного числа черточек в случаях, когда разность равна 0 :)
     for (i = 1; i <= numlen(result); ++i)
     {
         if (i == 1)
@@ -28,10 +28,9 @@ void quotient_out(int M, int N)
             printf("|%i\n", N);
         }
 
-        print_n_symbols(numlen(indent) - numlen(N * n_th_dig_of_num(i, result)), ' ');
-
-
-        printf("%i", N * n_th_dig_of_num(i, result));
+        product = N * n_th_dig_of_num(i, result);
+        print_n_symbols(numlen(indent) - numlen(product), ' ');
+        printf("%i", product);
 
         if (i != 1)
             printf("\n");
@@ -43,30 +42,32 @@ void quotient_out(int M, int N)
             printf("%i\n", result);
         }
 
-        int x;
+        int num_of_additional_spaces;
 
         if (i != 1)
-            print_n_symbols(x, ' ');
+            print_n_symbols(num_of_additional_spaces, ' ');
 
-
-        print_n_symbols(numlen(dividend), '-');
+        if (crutch == 0)
+            print_n_symbols(numlen(dividend) + 1, '-');
+        else
+            print_n_symbols(numlen(dividend), '-');
 
         printf("\n");
 
-        print_n_symbols(numlen(indent) - numlen(dividend - N * n_th_dig_of_num(i, result)), ' ');
+        print_n_symbols(numlen(indent) - numlen(dividend - product), ' ');
 
-        x = numlen(indent) - numlen(dividend - N * n_th_dig_of_num(i, result));
+        num_of_additional_spaces = numlen(indent) - numlen(dividend - product);
 
-        printf("%i", dividend - N * n_th_dig_of_num(i, result));
+        printf("%i", dividend - product);
         if (i == numlen(result))
             printf("\n");
 
         if (i != numlen(result))
             printf("%i\n", n_th_dig_of_num(i, residue));
 
-        dividend = (dividend - N * n_th_dig_of_num(i, result)) * 10 + n_th_dig_of_num(i, residue);
+        crutch = dividend - product;
+        dividend = (dividend - product) * 10 + n_th_dig_of_num(i, residue);
         indent = indent * 10 + n_th_dig_of_num(i, residue);
 
     }
 }
-
