5a4b91eee4d23d6bbd655b651b5fbb603e090dd0
Anton <antonlamtev@gmail.com>
2015-11-22 21:29:53 +0300

    добавил тесты для queens. Есть некоторые проблемы объявлением функций в тестах

--- sources/subdirproject/qt_tests/tst_qt_teststest.cpp
+++ sources/subdirproject/qt_tests/tst_qt_teststest.cpp
@@ -1,7 +1,11 @@
 #include <QString>
 #include <QtTest>
+#include <stdlib.h>
 //#include "exchange_change.h"
 #include "exchange_structures_declaration.h"
+#include "queens_operands_declaration.h"
+#include "queens_result.h"
+#include "queens_check_for_beating.h"
 
 //если раскомментить //#include "exchange_change.h" и закоментить функцию ниже, то undefined reference у change_by_coins(int)
 struct purse change_by_coins(int amount)
@@ -16,6 +20,59 @@ struct purse change_by_coins(int amount)
     return coins;
 }
 
+//такая же проблема. пока не знаю, как решить
+int queens_result(struct coordinate x, struct coordinate y)
+{
+    int result = no_one;
+    if (check_for_beating(x.first, y.first, x.second, y.second))
+    {
+        if (check_for_beating(x.first, y.first, x.third, y.third))
+        {
+            if (check_for_beating(x.third, y.third, x.second, y.second))
+            {
+                result = everyone;
+            }
+            else
+            {
+                result = OneTwo_OneThree;
+            }
+        }
+        else
+        {
+            if (check_for_beating(x.third, y.third, x.second, y.second))
+            {
+                result = OneTwo_TwoThree;
+            }
+            else
+            {
+                result = OneTwo;
+            }
+        }
+    }
+    else if (check_for_beating(x.first, y.first, x.third, y.third))
+    {
+        if (check_for_beating(x.third, y.third, x.second, y.second))
+        {
+            result = OneThree_TwoThree;
+        }
+        else
+        {
+            result = OneThree;
+        }
+    }
+    else if (check_for_beating(x.third, y.third, x.second, y.second))
+    {
+        result = TwoThree;
+    }
+    return result;
+}
+
+int check_for_beating(int a, int b, int c, int d)
+{
+    return (a == c || b == d) || (abs(a-c) == abs(b-d));
+
+}
+
 class Qt_testsTest : public QObject
 {
     Q_OBJECT
@@ -25,8 +82,8 @@ public:
 
 private Q_SLOTS:
     void exchange_test();
-    /*void queens_test();
-    void quotient_test();
+    void queens_test();
+    /*void quotient_test();
     void matrix_test();
     void lines_simmetrization_test();*/
 };
@@ -57,6 +114,18 @@ void Qt_testsTest::exchange_test()
     QCOMPARE(coins2_real.twos, coins2_expected.twos);
     QCOMPARE(coins2_real.ones, coins2_expected.ones);
 
+}
+
+void Qt_testsTest::queens_test()
+{
+    struct coordinate x_real, y_real;
+    x_real.first = 1;
+    y_real.first = 2;
+    x_real.second = 2;
+    y_real.second = 3;
+    x_real.third = 3;
+    y_real.third = 4;
+    QCOMPARE(queens_result(x_real, y_real), 1);
 }
 
 QTEST_APPLESS_MAIN(Qt_testsTest)
