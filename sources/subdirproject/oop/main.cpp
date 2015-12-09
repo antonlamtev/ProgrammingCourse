6c9c2b54cb46738008b1452e52eb83289a77c341
Anton <antonlamtev@gmail.com>
2015-12-09 16:05:14 +0300

    подчистил main

--- sources/subdirproject/oop/main.cpp
+++ sources/subdirproject/oop/main.cpp
@@ -1,13 +1,9 @@
-#include "table.h"
 #include "table_app.h"
 
 int main()
 {
     Table_app app;
     app.menu();
-//    Table table;
-//    table.put("sadfs", 128);
-//    cout << table[128] << endl;
     return 0;
 }
 
