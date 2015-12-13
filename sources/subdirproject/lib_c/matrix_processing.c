b11d2dc3977715edd5dca24e1e7fa2b19d224a83
Anton <antonlamtev@gmail.com>
2015-12-13 19:01:37 +0300

    подправил код. отчет по 4-м заданиям

--- sources/subdirproject/lib_c/matrix_processing.c
+++ sources/subdirproject/lib_c/matrix_processing.c
@@ -1,8 +1,8 @@
-int level_of_null(int** P, int n, int number_of_column)
+int level_of_null(int** P, int n, int number_of_line)
 {
     int result = 0;
     for (int i = 0; i < n; ++i)
-        if (P[number_of_column][i] == 0)
+        if (P[number_of_line][i] == 0)
         {
             result = i;
             break;
@@ -17,7 +17,8 @@ void sort_nulls_to_the_main_diagonal(int** P, int n)
     int i, j;
     for (i = n-1; i >= 0; --i)
         for (j = 0; j < i; ++j)
-            if (level_of_null(P, n, j) > level_of_null(P, n, j+1)){
+            if (level_of_null(P, n, j) > level_of_null(P, n, j+1))
+            {
                     t = P[j];
                     P[j] = P[j+1];
                     P[j+1] = t;
